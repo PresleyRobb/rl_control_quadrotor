1a3f1eb1f0f5cb904594f4f4cb55779b00ddeaf7
Presley Robb <robbp1@duq.edu>
2026-03-13 11:24:48 -0400

    Initial commit

---
+++ ROS 2/uav_control/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'uav_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='presrobb',
+    maintainer_email='robbp1@duq.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'm1 = uav_control.m1:main'
+        ],
+    },
+)
